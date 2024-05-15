65f1e57f1c8b4ee66b1df928f583444a81047ec4
gangabiswa <Gbiswa@pgh.year.org>
2024-05-14 22:49:20 -0400

    Worked dealershipFileManager

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,64 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    ArrayList<Vehicle>inventory;
+
+    public Dealership(String name, String address, String phone, List<Vehicle> vehicles) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return inventory;
+    }
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+        return  inventory;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(double min, double max) {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String type) {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+    public void addVehicles(Vehicle vehicle){
+        inventory.add(vehicle);
+    }
+    public void removeVehicles(Vehicle vehicle){
+        inventory.remove(vehicle);
+    }
+
 }
--- src/main/java/com/pluralsight/DealershipFilesManager.java
+++ src/main/java/com/pluralsight/DealershipFilesManager.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DealershipFilesManager {
+    public static Dealership getDealership(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            String[] dealershipInfo = null;
+            List<Vehicle> vehicles = new ArrayList<>();
+
+
+            if ((line = reader.readLine()) != null) {
+                dealershipInfo = line.split("\\|");
+            }
+            while ((line = reader.readLine()) != null) {
+                String[] vehicleData = line.split("\\|");
+                Vehicle vehicle = new Vehicle(vehicleData[0], vehicleData[1], Double.parseDouble(vehicleData[2]));
+                vehicles.add(vehicle);
+            }
+            if (dealershipInfo != null && dealershipInfo.length == 3) {
+                return new Dealership(dealershipInfo[0], dealershipInfo[1], dealershipInfo[2], vehicles);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void saveDealership(Dealership dealership, String filePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhone());
+            writer.newLine();
+
+            for (Vehicle vehicle : dealership.getAllVehicles()) {
+                writer.write(vehicle.getMake() + "|" + vehicle.getModel() + "|" + vehicle.getPrice() + "|...");
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
