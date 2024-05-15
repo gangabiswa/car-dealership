9041244807ef806490b91d8b6e5bd44118691dac
gangabiswa <Gbiswa@pgh.year.org>
2024-05-14 22:58:34 -0400

    added Attributes and construckter to Userinterface

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private  Dealership dealership;
+    private Scanner scanner;
+
+    public UserInterface(Dealership dealership, Scanner scanner) {
+        this.dealership = dealership;
+        this.scanner = new Scanner(System.in);
+    }
+    
 }
